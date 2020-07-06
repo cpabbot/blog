1e65686b333983e6bf8c6c93a288ab234f6c9d80
Cameron Abbot <cpabbot1@gmail.com>
2020-07-06 00:14:37 -0400

    basic groundwork

---
+++ js/fade-in.js
@@ -0,0 +1,28 @@
+(function() {
+    var elements;
+    var windowHeight;
+  
+    function init() {
+      elements = document.querySelectorAll('.scroll-fade');
+      windowHeight = window.innerHeight;
+    }
+  
+    function checkPosition() {
+      for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        var positionFromTop = elements[i].getBoundingClientRect().top;
+        
+        // console.log(i + ": " + positionFromTop + " - " + windowHeight);
+        if (positionFromTop - windowHeight <= -200) {
+          element.classList.add('fade-in');
+          element.classList.remove('scroll-fade');
+        }
+      }
+    }
+
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+    
+    init();
+    checkPosition();
+  })();
