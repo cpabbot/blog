d69320b54ebdbb7918d4a5f74c1ed16594f60e84
Cameron Abbot <cpabbot1@gmail.com>
2020-07-27 16:09:25 -0400

    basic navigation js created

--- js/theme-mode.js
+++ js/theme-mode.js
@@ -6,13 +6,13 @@ function toggleThemeMode() {
     if(theme === 'light') {
         element.classList.remove('light-theme');
         element.classList.add('dark-theme');
-        icon.src = 'img/icons/sun.svg';
+        icon.src = '/img/icons/sun.svg';
         theme = 'dark';
     }
     else if(theme === 'dark') {
         element.classList.remove('dark-theme');
         element.classList.add('light-theme');
-        icon.src = 'img/icons/night.svg';
+        icon.src = '/img/icons/night.svg';
         theme = 'light';
     }
 }
